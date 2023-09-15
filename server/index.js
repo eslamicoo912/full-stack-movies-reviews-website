c2714e601b3e0ebeec26837b8f59b82e568418bb
eslamicoo912 <eslamicoo3@gmail.com>
2023-09-16 01:35:21 +0300

    setting up the server

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
+const db_url = process.env.DB_URL;
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mbs", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mbs", extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello, Cinema!");
+});
+
+mongoose
+  .connect(db_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`listening for port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
